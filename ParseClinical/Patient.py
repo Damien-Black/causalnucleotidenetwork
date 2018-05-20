c13ab6c7d6e4e8c33edc5f352e51ccdecabc0fec
Steve Osazuwa <sosazuwa00@gmail.com>
2018-05-20 01:21:22 -0700

    Expose more on patient class

--- ParseClinical/Patient.py
+++ ParseClinical/Patient.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals, division
 from collections import OrderedDict
+import utils
 
 
 class Patient(OrderedDict):
@@ -7,20 +8,54 @@ class Patient(OrderedDict):
 
     Constructor:
         clinical_obj `OrderedDict`: TCGA clinical XML as ordered dictionary
+
+    Todo:
+        - Use utils.get_or_default in place of try.. except KeyError logic
     """
     def __init__(self, clinical_obj):
         self.patient_uuid = clinical_obj['kirp:patient']['shared:bcr_patient_uuid']['#text']
         super(Patient, self).__init__(clinical_obj['kirp:patient'])
         self._age = None
-        self._survival_time = None
         self._censored = None
+        self._clinical_stage = None
+        self._gender = None
+        self._pathologic_stage = None
+        self._survival_time = None
 
     @property
     def age(self):
         if self._age is None:
-            age_data = self["clin_shared:days_to_birth"]["#text"]
-            self._age = -int(age_data) / 365.25
+            age_data = utils.get_or_default(self["clin_shared:days_to_birth"], '#text')
+            if age_data != 'UNK':
+                age_data = -int(age_data) / 365.25
+            self._age = age_data
         return self._age
+
+    @property
+    def censored(self):
+        if self._censored is None:
+            _ = self.survival_time  # censored is set here
+        return self._censored
+
+    @property
+    def clinical_stage(self):
+        if self._clinical_stage is None:
+            self._clinical_stage = utils.get_or_default(
+                self['shared_stage:stage_event']['shared_stage:clinical_stage'], '#text')
+        return self._clinical_stage
+
+    @property
+    def gender(self):
+        if self._gender is None:
+            self._gender = utils.get_or_default(self['shared:gender'], '#text')
+        return self._gender
+
+    @property
+    def pathologic_stage(self):
+        if self._pathologic_stage is None:
+            self._pathologic_stage = utils.get_or_default(
+                self['shared_stage:stage_event']['shared_stage:pathologic_stage'], '#text')
+        return self._pathologic_stage
 
     @property
     def survival_time(self):
@@ -40,9 +75,3 @@ class Patient(OrderedDict):
                 self._censored = False
 
         return self._survival_time
-
-    @property
-    def censored(self):
-        if self._censored is None:
-            _ = self.survival_time  # censored is set here
-        return self._censored
